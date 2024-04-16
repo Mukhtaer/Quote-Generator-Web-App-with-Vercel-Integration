0a38976832aca49cf11490cf4a2c68e35eb3005e
Mukhtar Sani <mukhtarsani20@gmail.com>
2024-04-16 12:37:47 +0800

    updated favicon route

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
-import os
-from flask import Flask, render_template, send_from_directory    
+
+from flask import Flask, render_template   
 import random
 
 app = Flask(__name__)
@@ -190,9 +190,9 @@ def index():
     quote = random.choice(quotes)
     return render_template('index.html', quote=quote)
 
-@app.route('/favicon.ico') 
-def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route("/favicon.ico")
+def favicon():
+    return url_for('static', filename='data:,')
 
 if __name__ == '__main__':
     app.run(debug=True)
